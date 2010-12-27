9395a3b3fcea1816a3b2552c5b8faeb6a8900984
Alistair McLean <al.mclean@gmail.com>
2010-12-27 12:09:52 +0000

    Added LWJGL and got basic window to display

--- src/com/maoni/Main.java
+++ src/com/maoni/Main.java
@@ -1,9 +1,39 @@
 package com.maoni;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL33;
+
 public class Main {
 	
 	public static void main (String args[]) {
 		System.out.println("Hello world !");
+		init();
+		
+		while (!Display.isCloseRequested()) {
+			render();
+			Display.update();
+		}
+		Display.destroy();
+	}
+	
+	private static void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void init() {
+		try {
+			
+			Display.setDisplayMode(new DisplayMode(800, 600));
+			Display.setVSyncEnabled(true);
+			Display.setTitle("Maoni V0.01");
+			Display.create();
+			
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
